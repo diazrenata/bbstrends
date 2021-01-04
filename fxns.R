937c77baa8d5189a2b958ac13dfdc74111d094e6
Renata Diaz <diaz.renatam@gmail.com>
2021-01-04 15:51:40 -0500

    cleaning to start fromwork in BBSStories

--- fxns.R
+++
@@ -1,8 +0,0 @@
-get_bbs_totalabund <- function(a_bbs) {
-  return(data.frame(
-    timestep = a_bbs$covariates[ , a_bbs$metadata$timename],
-    totalabund = rowSums(a_bbs$abundance),
-    route = a_bbs$metadata$route,
-    region = a_bbs$metadata$region
-  ))
-}
